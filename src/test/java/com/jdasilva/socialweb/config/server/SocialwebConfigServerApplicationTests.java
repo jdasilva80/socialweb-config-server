fa1be9fd74572c386d40f13aae43a3f5976439b6
jdasilva80 <jdasilva1980@gmail.com>
2020-06-30 17:02:23 +0200

    primer commit

---
+++ src/test/java/com/jdasilva/socialweb/config/server/SocialwebConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdasilva.socialweb.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialwebConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
